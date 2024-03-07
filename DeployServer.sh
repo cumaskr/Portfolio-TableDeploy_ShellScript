4c4d69dad320b5194e4eec47c7dd115cdda5e353
whjeonlocal <whjeonlocal@gmail.com>
2024-03-07 13:41:14 +0900

    [전우형] 쉘스크립트 수정(서버 패치)

--- DeployServer.sh
+++ DeployServer.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-echo "[Test] Table Server Shell Script"
+echo "[ShellScript] Stop Service"
+sudo systemctl stop APIServer.service
+echo "[ShellScript] Git Pull"
+cd ../Portfolio-TableDeploy_Server
+sudo git pull
+echo "[ShellScript] Project Build"
+sudo dotnet build APIServer.csproj
+echo "[ShellScript] Start Service"
+sudo systemctl start APIServer.service
+echo "[ShellScript] Complete"
